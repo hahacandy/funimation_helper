effe59c5fe31aeb4e3fc17f7f3aa693fc37c57c3
hahacandy <58638224+hahacandy@users.noreply.github.com>
2023-12-25 06:32:02 +0900

    Update trans_server.py

--- youtube/trans_server.py
+++ youtube/trans_server.py
@@ -20,7 +20,7 @@ def get_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-setuid-sandbox")
     options.add_argument('--disable-gpu')
-    #options.add_argument('headless')
+    options.add_argument('headless')
     
     driver = webdriver.Chrome(options=options)
     
